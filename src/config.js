ea06a68b1a6938756949f0976baec8984171e423
QuizSnight <s.weberbn1986@gmail.com>
2026-06-01 00:18:03 +0200

    Configure Google Sheets webhook

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ window.T2E_CONFIG = {
   supabaseUrl: "https://iasvzuadtjdqrfdilyun.supabase.co",
   supabaseAnonKey: "sb_publishable_30t1T306Yq-vSOVUNLKdsw_rcYQgjTm",
   teamId: "ksch-spiele",
-  googleSheetsWebhookUrl: "",
+  googleSheetsWebhookUrl: "https://script.google.com/macros/s/AKfycbw3uYZ9y_GeKtpsoIdsizPVoFKVgjYe7gCrrhslpPD8LL-atABptitvo0jG2hAUAbqebA/exec",
 };
